ad9a39ddb05913136f9b95c0964b7b0d9c3452c8
PowertrainFSAE47 <dominicis.a@gmail.com>
2021-06-18 20:27:06 -0400

    Integrada interfaz con estilos.

---
+++ HormesaFILEIDS/MyAddinControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using static AngelSix.SolidDna.SolidWorksEnvironment;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Threading;
+using SharpDX.XInput;
+using System;
+using HormesaFILEIDS.ViewModel;
+
+namespace AddinTesting
+{
+    /// <summary>
+    /// Interaction logic for MyAddinControl.xaml
+    /// </summary>
+    public partial class MyAddinControl : UserControl
+    {
+        //DATACONTEXT.
+        private viewModel vm;
+
+
+        #region Constructor
+
+
+        public MyAddinControl()
+        {
+
+            //El datacontext es el viewmodel, el viewmodel se comunica con todas las otras clases 
+            vm = new viewModel();
+            this.DataContext = vm;
+            vm.initVM();
+            InitializeComponent();
+        }
+
+        #endregion
+
+    }
+}
---
+++ HormesaFILEIDS/ViewModel/viewModel.cs
@@ -0,0 +1,49 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+
+namespace HormesaFILEIDS.ViewModel
+{
+
+    public class viewModel : INotifyPropertyChanged
+    {
+
+        #region private fields
+        private SldWorks swApp;
+
+        #endregion
+
+        #region Inicializar
+
+        public void initVM()
+        {
+            try
+            {
+                // Obtener Instancia activa de solidworks
+                swApp = (SldWorks)System.Runtime.InteropServices.Marshal.GetActiveObject("SldWorks.Application");
+
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error en inicializacion de viewModel: " + e.Message);
+            }
+        }
+
+        #endregion
+
+
+
+        #region Implementar la interfaz InotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
